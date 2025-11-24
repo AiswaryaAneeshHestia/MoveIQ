48bd3994f0e7ff243cb7a3219b87abd650632db0
AiswaryaAneeshHestia <Aiswarya_aneesh@hestiatechnology.com>
2025-11-24 10:04:32 +0530

    updated customer list

--- src/pages/customer/List.tsx
+++ src/pages/customer/List.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useCallback } from "react";
-import { useNavigate } from "react-router-dom";
 import type { Customer } from "../../types/Customer.types";
 import CustomerService from "../../services/Customer.services";
 import KiduLoader from "../../components/KiduLoader";
